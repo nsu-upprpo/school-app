8f8043ebee72d049f9189235a6d0effa24309d85
ATaskaev <ataskaev@yandex.ru>
2026-03-12 18:08:26 +0700

    init gradle with spring

---
+++ backend/src/main/java/com/github/nsu_upprpo/school_app/SchoolAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.nsu_upprpo.school_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/nsu_upprpo/school_app/SchoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nsu_upprpo.school_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
