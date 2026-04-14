d1906182420b1e4596b12236810ea15719a15a9e
ATaskaev <ataskaev@yandex.ru>
2026-04-14 09:36:03 +0700

    fix(backend): add header to security config, so h2 console works

--- backend/src/main/java/com/github/nsu_upprpo/school_app/config/SecurityConfig.java
+++ backend/src/main/java/com/github/nsu_upprpo/school_app/config/SecurityConfig.java
@@ -24,6 +24,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.sameOrigin())
+                )
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
